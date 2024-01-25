4e9982ef30d9b04ee4ee24cbbb5da945433dfcc4
esther_chen <esther_chen@trendmicro.com>
2024-01-25 17:17:44 +0800

    init commit

---
+++ app/src/main/java/com/esther/calculator/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.esther.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.esther.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    /*
+                    * C,+/-,%, ÷
+                    * 7, 8, 9, ×
+                    * 4, 5, 6, -
+                    * 1, 2, 3, +
+                    * 0,  , ., =
+                    * */
+                }
+            }
+        }
+    }
+
+}
